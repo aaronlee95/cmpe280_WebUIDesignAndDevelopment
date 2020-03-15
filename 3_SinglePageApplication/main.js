f2da2497d6dbbba44f4e12ff54e0417f35c39eb9
Aaron Lee <aaron.k.lee1@gmail.com>
2020-03-14 23:28:19 -0700

    basic navigation for SPA and zip files for assignment 1 and 2

---
+++ 3_SinglePageApplication/main.js
@@ -0,0 +1,42 @@
+// Returns the html link to each page
+function getContent(fragmentID, callback){
+  var request = new XMLHttpRequest();
+  request.onload = function(){
+    callback(request.responseText);
+  };
+
+  request.open("GET", fragmentID + ".html");
+  request.send(null);
+}
+
+// function setActiveLink(fragmentID){
+//   navbarDiv = document.getElementById("navbar");
+//   links = navbarDiv.children;
+//   // var i, link, pageName;
+
+//   for(i=0; i < links.length; i++){
+//     link = links[i];
+//     // pageName = link.getAttribute("href").substr(1);
+//     if(pageName == fragmentID){
+//       console.log("setting to active: " + pageName);
+//       link.setAttribute("class", "active");
+//     }
+//     else{
+//       link.removeAttribute("class");
+//     }
+//   }
+// }
+
+function navigate(){
+  var contentDiv = document.getElementById("content");
+  var fragmentID = location.hash.substr(1);
+  console.log(fragmentID);
+  getContent(fragmentID, function(content){
+    contentDiv.innerHTML=content;
+  });
+  // setActiveLink(fragmentID);
+};
+
+navigate();
+window.addEventListener("hashchange", navigate);
+
